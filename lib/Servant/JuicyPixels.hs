ee2c3602f46527348536a9bb36cc59ca361fd422
3kyro <k.papachrisanthou@gmail.com>
2021-06-21 11:34:23 +0200

    Discard JPEG_METADATA and add further instances
    Missing `MimeRender` instances (eg. GIF, TIFF etc) are due to
    JuicyPixels not providing a `decodeTYPEWithMetadata` function.

--- lib/Servant/JuicyPixels.hs
+++ lib/Servant/JuicyPixels.hs
@@ -1,27 +1,32 @@
-{-# LANGUAGE CPP                   #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE KindSignatures        #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Servant.JuicyPixels where
 
-import Servant.API
+import Codec.Picture
+import Codec.Picture.Bitmap
+import Codec.Picture.Gif
+import Codec.Picture.HDR
+import Codec.Picture.Jpg
+import Codec.Picture.Metadata
+import Codec.Picture.Png
+import Codec.Picture.Saving
+import Codec.Picture.Tga
+import Codec.Picture.Tiff
+import Codec.Picture.Types
+import qualified Data.ByteString.Lazy as BL
+import Data.Proxy
 import GHC.TypeLits
 import qualified Network.HTTP.Media as M
-import Codec.Picture
-import Codec.Picture.Metadata
-import Codec.Picture.Jpg
-import Codec.Picture.Saving
-import Codec.Picture.Types
-import Data.Proxy
-import qualified Data.ByteString.Lazy as BL
-
+import Servant.API
 
 data BMP
 
@@ -31,9 +36,14 @@ instance Accept BMP where
 instance MimeRender BMP DynamicImage where
     mimeRender _ = imageToBitmap
 
+instance BmpEncodable pixel => MimeRender BMP (Image pixel, Metadatas) where
+    mimeRender _ (img, metadata) = encodeBitmapWithMetadata metadata img
+
 instance MimeUnrender BMP DynamicImage where
     mimeUnrender _ = decodeBitmap . BL.toStrict
 
+instance MimeUnrender BMP (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodeBitmapWithMetadata . BL.toStrict
 
 data GIF
 
@@ -46,6 +56,8 @@ instance MimeRender GIF DynamicImage where
 instance MimeUnrender GIF DynamicImage where
     mimeUnrender _ = decodeGif . BL.toStrict
 
+instance MimeUnrender GIF (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodeGifWithMetadata . BL.toStrict
 
 data JPEG (quality :: Nat)
 
@@ -61,28 +73,15 @@ instance (KnownNat quality, quality <= 100, ColorSpaceConvertible a PixelYCbCr8)
     mimeRender _ = encodeJpegAtQuality quality . convertImage
       where quality = fromInteger $ natVal (Proxy :: Proxy quality)
 
+instance (KnownNat quality, quality <= 100, ColorSpaceConvertible a PixelYCbCr8) => MimeRender (JPEG quality) (Image a, Metadatas) where
+    mimeRender _ (img, metadata) = encodeJpegAtQualityWithMetadata quality metadata $ convertImage img
+      where
+        quality = fromInteger $ natVal (Proxy :: Proxy quality)
+
 instance (KnownNat quality, quality <= 100) => MimeUnrender (JPEG quality) DynamicImage where
     mimeUnrender _ = decodeJpeg . BL.toStrict
 
-data JPEG_METADATA (quality :: Nat)
-
-instance (KnownNat quality, quality <= 100) => Accept (JPEG_METADATA quality) where
-    contentType _ = "image" M.// "jpeg"
-
-instance (KnownNat quality, quality <= 100) => MimeRender (JPEG_METADATA quality) DynamicImage where
-    mimeRender _ img =
-      let quality = fromInteger $ natVal (Proxy :: Proxy quality)
-      in imageToJpg quality img
-
-instance (KnownNat quality, quality <= 100, ColorSpaceConvertible a PixelYCbCr8) => MimeRender (JPEG_METADATA quality) (Image a) where
-    mimeRender _ = encodeJpegAtQuality quality . convertImage
-      where quality = fromInteger $ natVal (Proxy :: Proxy quality)
-
-instance (KnownNat quality, quality <= 100, ColorSpaceConvertible a PixelYCbCr8) => MimeRender (JPEG_METADATA quality) (Image a, Metadatas) where
-    mimeRender _ (img,metadata) = encodeJpegAtQualityWithMetadata quality metadata $ convertImage img
-      where quality = fromInteger $ natVal (Proxy :: Proxy quality)
-
-instance (KnownNat quality, quality <= 100) => MimeUnrender (JPEG_METADATA quality) (DynamicImage, Metadatas) where
+instance (KnownNat quality, quality <= 100) => MimeUnrender (JPEG quality) (DynamicImage, Metadatas) where
     mimeUnrender _ = decodeJpegWithMetadata . BL.toStrict
 
 data PNG
@@ -96,9 +95,14 @@ instance MimeRender PNG DynamicImage where
 instance PngSavable a => MimeRender PNG (Image a) where
     mimeRender _ = encodePng
 
+instance PngSavable a => MimeRender PNG (Image a, Metadatas) where
+    mimeRender _ (img, metadata) = encodePngWithMetadata metadata img
+
 instance MimeUnrender PNG DynamicImage where
     mimeUnrender _ = decodePng . BL.toStrict
 
+instance MimeUnrender PNG (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodePngWithMetadata . BL.toStrict
 
 data TIFF
 
@@ -114,6 +118,8 @@ instance TiffSaveable a => MimeRender TIFF (Image a) where
 instance MimeUnrender TIFF DynamicImage where
     mimeUnrender _ = decodeTiff . BL.toStrict
 
+instance MimeUnrender TIFF (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodeTiffWithMetadata . BL.toStrict
 
 data RADIANCE
 
@@ -129,6 +135,9 @@ instance a ~ PixelRGBF => MimeRender RADIANCE (Image a) where
 instance MimeUnrender RADIANCE DynamicImage where
     mimeUnrender _ = decodeHDR . BL.toStrict
 
+instance MimeUnrender RADIANCE (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodeHDRWithMetadata . BL.toStrict
+
 #if MIN_VERSION_JuicyPixels(3,2,6)
 data TGA
 
@@ -143,4 +152,7 @@ instance TgaSaveable a => MimeRender TGA (Image a) where
 
 instance MimeUnrender TGA DynamicImage where
     mimeUnrender _ = decodeTga . BL.toStrict
+
+instance MimeUnrender TGA (DynamicImage, Metadatas) where
+    mimeUnrender _ = decodeTgaWithMetadata . BL.toStrict
 #endif
